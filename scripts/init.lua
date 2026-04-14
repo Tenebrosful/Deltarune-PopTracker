570f71f09ce56de1a74f9ea5a210e767ad63099a
Tenebrosful <tenebrosful@proton.me>
2026-04-14 18:17:00 +0200

    Autobuild from ap

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,44 +1,44 @@
--- entry point for all lua code of the pack
--- more info on the lua API: https://github.com/black-sliver/PopTracker/blob/master/doc/PACKS.md#lua-interface
-ENABLE_DEBUG_LOG = true
--- get current variant
+
 local variant = Tracker.ActiveVariantUID
--- check variant info
-IS_ITEMS_ONLY = variant:find("itemsonly")
-
-print("-- Example Tracker --")
-print("Loaded variant: ", variant)
-if ENABLE_DEBUG_LOG then
-    print("Debug logging is enabled!")
-end
-
--- Utility Script for helper functions etc.
-ScriptHost:LoadScript("scripts/utils.lua")
-
--- Logic
-ScriptHost:LoadScript("scripts/logic/logic.lua")
-
--- Custom Items
-ScriptHost:LoadScript("scripts/custom_items/class.lua")
-ScriptHost:LoadScript("scripts/custom_items/progressiveTogglePlus.lua")
-ScriptHost:LoadScript("scripts/custom_items/progressiveTogglePlusWrapper.lua")
 
 -- Items
-Tracker:AddItems("items/items.jsonc")
+require("scripts/items_import")
 
-if not IS_ITEMS_ONLY then -- <--- use variant info to optimize loading
-    -- Maps
-    Tracker:AddMaps("maps/maps.jsonc")
-    -- Locations
-    Tracker:AddLocations("locations/locations.jsonc")
+-- Logic
+require("scripts/logic/logic_helper")
+require("scripts/logic/base_logic")
+require("scripts/logic/graph_logic/logic_main")
+
+-- Maps
+if Tracker.ActiveVariantUID == "maps-u" then
+    Tracker:AddMaps("maps/maps-u.json")  
+else
+    Tracker:AddMaps("maps/maps.json")  
+end  
+
+if PopVersion and PopVersion >= "0.23.0" then
+    Tracker:AddLocations("locations/dungeons.json")
 end
 
 -- Layout
-Tracker:AddLayouts("layouts/items.jsonc")
-Tracker:AddLayouts("layouts/tracker.jsonc")
-Tracker:AddLayouts("layouts/broadcast.jsonc")
+require("scripts/layouts_import")
+
+-- Locations
+require("scripts/locations_import")
 
 -- AutoTracking for Poptracker
-if PopVersion and PopVersion >= "0.18.0" then
-    ScriptHost:LoadScript("scripts/autotracking.lua")
+if PopVersion and PopVersion >= "0.26.0" then
+    require("scripts/autotracking")
 end
+
+function OnFrameHandler()
+    ScriptHost:RemoveOnFrameHandler("load handler")
+    -- stuff
+    ScriptHost:AddWatchForCode("StateChanged", "*", StateChanged)
+    ScriptHost:AddOnLocationSectionChangedHandler("location_section_change_handler", LocationHandler)
+    CreateLuaManualStorageItem("manual_location_storage")
+    ForceUpdate()
+end
+require("scripts/luaitems")
+require("scripts/watches")
+ScriptHost:AddOnFrameHandler("load handler", OnFrameHandler)
